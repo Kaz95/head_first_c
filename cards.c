236b386cc351a67baed58d2210434cfb9815d0cb
kaz95 <smurfrektshrekt@gmail.com>
2022-03-17 00:11:13 -0500

    Finished loop exercise from pg. 35

--- cards.c
+++ cards.c
@@ -14,26 +14,34 @@
 int main()
 {
 	char card_name[3];
-	puts("Enter the card_name: ");
-	scanf("%2s", card_name);
-	int val = 0;
-	switch(card_name[0]) {
-	case 'K':
-	case 'Q':
-	case 'J':
-		val = 10;
-		break;
-	case 'A':
-		val = 11;
-		break;
-	default:
-		val = atoi(card_name);
+	int count = 0;
+	while (card_name[0] != 'X') {
+		puts("Enter the card_name: ");
+		scanf("%2s", card_name);
+		int val = 0;
+		switch(card_name[0]) {
+			case 'K':
+			case 'Q':
+			case 'J':
+				val = 10;
+				break;
+			case 'A':
+				val = 11;
+				break;
+			case 'X':
+				continue;
+			default:
+				val = atoi(card_name);
+				if ((val< 2) || (val > 10)) {
+					puts("Invalid Input! Try a Real Card!");
+					continue;
+				}
+		}
+		if ((val > 2) && (val < 7))
+			count++;
+		else if (val == 10)
+			count--;
+		printf("The Count is: %i\n", count);	
 	}
-	
-	if ((val > 2) && (val < 7))
-		puts("Count has gone up");
-	else if (val == 10)
-		puts("Count has gone down");
-
 	return 0;
 }
